c61786c3695fda358792ef6a874b47de623930cf
Isaac <ijharvey456@gmail.com>
2025-11-16 01:50:37 -0600

    Scaffold Skraps route

--- site/app/skraps/page.tsx
+++ site/app/skraps/page.tsx
@@ -4,3 +4,11 @@ import Link from 'next/link';
 export default function SkrapsPage() {
     return <></>;
 }
+
+/**
+ * Plan for Skraps DAW
+ * - data model is important. I like Jazz - anonymous accounts with optional conversion.
+ * Even though this is on loop-kit site its fine. I can use loop-kit accounts for skraps for now.
+ * conversion later.
+ *
+ */
