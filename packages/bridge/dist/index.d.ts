b6bad67dc4303df1c4f06d78d5df1efb6d0d4b4f
Isaac <ijharvey456@gmail.com>
2026-03-04 13:21:32 -0600

    chore(shell): remove generated dist artifacts

--- packages/bridge/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './envelope.js';
-export * from './namedPipe.js';
-//# sourceMappingURL=index.d.ts.map
--- packages/bridge/dist/namedPipe.d.ts
+++
@@ -1,4 +0,0 @@
-import { type RpcResultEnvelope } from './envelope.js';
-export declare function namedPipePath(pipeName?: string): string;
-export declare function invokeNamedPipeRpc(pipePath: string, method: string, params?: Record<string, unknown>, timeoutMs?: number): Promise<RpcResultEnvelope>;
-//# sourceMappingURL=namedPipe.d.ts.map
